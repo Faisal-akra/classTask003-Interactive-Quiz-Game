d9727b1a8da4d3b92769c7bc04c69ef3f15a3d9d
Faisal-Akram <zjan45097@gmail.com>
2024-09-29 23:50:00 +0500

    create Object & question/amswer is input

--- java.js
+++ java.js
@@ -1,4 +1,108 @@
-var questionObject = ["islamabad","karachi","multan","lahore"]{
+var questionsObject = [
+    {
+        question: 'What does css stand for',
 
+        answer: [
+        'Cascading style sheet',
+        'Central processing unit',
+        'Hyper text markup language',
+        'Data object Model',
+
+    ],
+    correct: 'Cascading style sheet',
+    }
+]
+
+
+
+
+
+
+
+// },
+
+
+//     {
+//         question: 'What does DOM stand for',
+        
+//         answer: [
+//         'Cascading style sheet',
+//         'Central processing unit',
+//         'Hyper text markup language',
+//         'Data object Model',
+
+//     ],
+
+//     correct: 'Data object Model',
+// },
+
+
+
+
+//     {
+//         question: 'What does HTML stand for',
+        
+//         answer: [
+//         'Cascading style sheet',
+//         'Central processing unit',
+//         'Hyper text markup language',
+//         'Data object Model',
+
+//     ],
+
+//     correct: 'Hyper text markup language',
+// },
+
+
+
+
+//     {
+//         question: 'What does CPU stand for',
+        
+//         answer: [
+//         'Cascading style sheet',
+//         'Central processing unit',
+//         'Hyper text markup language',
+//         'Data object Model',
+//     ],
     
-}
+//         correct: 'Central processing unit',
+        
+// }
+// ];
+
+
+// var questionIndex = 0;
+
+// var questionArea = document.querySelector('.questionsArea h3');
+// var optionsArea = document.querySelectorAll('.btn');
+
+// function initializeQuiz() {
+//     questionArea.textContent = questionsObject[questionIndex].question;
+//     for (var i = 0; i < optionsArea.length; i++) {
+//         optionsArea[i].textContent = questionsObject[questionIndex].answer[i]
+//     }
+    
+// }
+
+// initializeQuiz();
+
+
+
+// for (var i = 0; i < optionsArea.length; i++) {
+
+//     optionsArea[i].addEventListener('click', checkedOption);
+// }
+
+// var obj = this;
+
+// function checkedOption() {
+//     var correctAnswer = questionsObject[questionIndex].correct;
+//     var selectedAnswer = this.textContent
+
+
+// }
+
+
+
+
