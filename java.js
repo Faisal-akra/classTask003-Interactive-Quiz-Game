0b88fdced9096b9357cc6b3d88643134eaf2c43c
Faisal-Akram <zjan45097@gmail.com>
2024-09-30 00:05:50 +0500

    css is almost complete

--- java.js
+++ java.js
@@ -10,98 +10,96 @@ var questionsObject = [
 
     ],
     correct: 'Cascading style sheet',
+    
+
+
+
+
+
+
+},
+
+
+    {
+        question: 'What does DOM stand for',
+        
+        answer: [
+        'Cascading style sheet',
+        'Central processing unit',
+        'Hyper text markup language',
+        'Data object Model',
+
+    ],
+
+    correct: 'Data object Model',
+},
+
+
+
+
+    {
+        question: 'What does HTML stand for',
+        
+        answer: [
+        'Cascading style sheet',
+        'Central processing unit',
+        'Hyper text markup language',
+        'Data object Model',
+
+    ],
+
+    correct: 'Hyper text markup language',
+},
+
+
+
+
+    {
+        question: 'What does CPU stand for',
+        
+        answer: [
+        'Cascading style sheet',
+        'Central processing unit',
+        'Hyper text markup language',
+        'Data object Model',
+    ],
+    
+        correct: 'Central processing unit',
+        
+}
+];
+
+
+var questionIndex = 0;
+
+var questionArea = document.querySelector('.questionsArea h3');
+var optionsArea = document.querySelectorAll('.btn');
+
+function initializeQuiz() {
+    questionArea.textContent = questionsObject[questionIndex].question;
+    for (var i = 0; i < optionsArea.length; i++) {
+        optionsArea[i].textContent = questionsObject[questionIndex].answer[i]
     }
-]
-
-
-
-
-
-
-
-// },
-
-
-//     {
-//         question: 'What does DOM stand for',
-        
-//         answer: [
-//         'Cascading style sheet',
-//         'Central processing unit',
-//         'Hyper text markup language',
-//         'Data object Model',
-
-//     ],
-
-//     correct: 'Data object Model',
-// },
-
-
-
-
-//     {
-//         question: 'What does HTML stand for',
-        
-//         answer: [
-//         'Cascading style sheet',
-//         'Central processing unit',
-//         'Hyper text markup language',
-//         'Data object Model',
-
-//     ],
-
-//     correct: 'Hyper text markup language',
-// },
-
-
-
-
-//     {
-//         question: 'What does CPU stand for',
-        
-//         answer: [
-//         'Cascading style sheet',
-//         'Central processing unit',
-//         'Hyper text markup language',
-//         'Data object Model',
-//     ],
     
-//         correct: 'Central processing unit',
-        
-// }
-// ];
+}
 
-
-// var questionIndex = 0;
-
-// var questionArea = document.querySelector('.questionsArea h3');
-// var optionsArea = document.querySelectorAll('.btn');
-
-// function initializeQuiz() {
-//     questionArea.textContent = questionsObject[questionIndex].question;
-//     for (var i = 0; i < optionsArea.length; i++) {
-//         optionsArea[i].textContent = questionsObject[questionIndex].answer[i]
-//     }
-    
-// }
-
-// initializeQuiz();
+initializeQuiz();
 
 
 
-// for (var i = 0; i < optionsArea.length; i++) {
+for (var i = 0; i < optionsArea.length; i++) {
 
-//     optionsArea[i].addEventListener('click', checkedOption);
-// }
+    optionsArea[i].addEventListener('click', checkedOption);
+}
 
-// var obj = this;
+var obj = this;
 
-// function checkedOption() {
-//     var correctAnswer = questionsObject[questionIndex].correct;
-//     var selectedAnswer = this.textContent
+function checkedOption() {
+    var correctAnswer = questionsObject[questionIndex].correct;
+    var selectedAnswer = this.textContent
 
 
-// }
+}
 
 
 
