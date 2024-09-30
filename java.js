e9e447032af077ff7b3d40357d32d040591a6b97
Faisal-Akram <zjan45097@gmail.com>
2024-09-30 23:03:33 +0500

    qusetion or options adjust in buttons

--- java.js
+++ java.js
@@ -10,14 +10,9 @@ var questionsObject = [
 
     ],
     correct: 'Cascading style sheet',
-    
+    },
 
 
-
-
-
-
-},
 
 
     {
@@ -70,11 +65,12 @@ var questionsObject = [
 ];
 
 
-var questionIndex = 0;
+var questionIndex = 0; // is ki madad say ham index 0 say start laingay:
 
 var questionArea = document.querySelector('.questionsArea h3');
 var optionsArea = document.querySelectorAll('.btn');
 
+//Question or options ko adjust kar liya buttons main
 function initializeQuiz() {
     questionArea.textContent = questionsObject[questionIndex].question;
     for (var i = 0; i < optionsArea.length; i++) {
@@ -85,21 +81,47 @@ function initializeQuiz() {
 
 initializeQuiz();
 
+// var marks = 0;
 
 
-for (var i = 0; i < optionsArea.length; i++) {
+// //Ab ham jo option correct hoga uspe color lagana chahtay hay
+// for (var i = 0; i < optionsArea.length; i++) {
+//     optionsArea[i].addEventListener('click', checkedOption);
+// }
 
-    optionsArea[i].addEventListener('click', checkedOption);
-}
+// var obj = this; // is ko hum nay idhar declare kiya
 
-var obj = this;
-
-function checkedOption() {
-    var correctAnswer = questionsObject[questionIndex].correct;
-    var selectedAnswer = this.textContent
+// function checkedOption() {
+//     var correctAnswer = questionsObject[questionIndex].correct;
+//     var selectedAnswer = this.textContent; // user jis option pe click karayga osko ham nay pakra
 
 
-}
+
+//     if (correctAnswer === selectedAnswer) {
+//         marks++;
+
+//         this.style.backgroundColor = "green";
+//         this.style.color = "white";
+//         this.textContent += '🎊'+'👏'
+//     }else{
+//         this.style.backgroundColor = "red";
+//         this.style.color = "white";
+//         this.textContent += '😢'
+
+//         //Agar user nay ghalat enter kiya to correct ka color bhi change kardo
+
+//         for (var i = 0; i < optionsArea.length; i++) {
+
+//             if (optionsArea[i].textContent === questionsObject[questionIndex].correct) {
+//                 optionsArea[i].style.backgroundColor = "green";
+//                 optionsArea[i].style.color = "white";
+//             }
+//         }
+      
+//     }
+
+
+// }
 
 
 
